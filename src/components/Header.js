052f14c6636b062eb17b2117947f0e965ce8d142
orozcoding <orozcoding@gmail.com>
2022-02-14 14:28:11 -0400

    start header and navbar

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => (
+  <header>
+    <div className="logo-container d-flex">
+      <p className="logo">Math Magicians</p>
+    </div>
+  </header>
+);
+
+export default Header;
