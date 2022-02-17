be85baac1e134c7c573347266af39eb6e6835536
joskalenda <joskalenda3@gmail.com>
2022-02-17 10:16:20 +0200

    Create testing for calculator file

--- src/__tests__/calculator.test.js
+++ src/__tests__/calculator.test.js
@@ -2,3 +2,14 @@ import React from 'react';
 import '@testing-library/react';
 import renderer from 'react-test-renderer';
 import Calculator from '../components/Calculator';
+
+describe('Renders calculator', () => {
+  it('renders', () => {
+    const tree = renderer
+      .create(
+        <Calculator />,
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
