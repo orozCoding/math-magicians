9536fd9da8b189f7b7c3786af4f3e7c0d3e94057
orozcoding <orozcoding@gmail.com>
2022-02-17 05:35:42 -0400

    add user interaction to App.test.js

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent, queryByAttribute } from '@testing-library/react';
 import renderer from 'react-test-renderer';
 import App from '../App';
 
@@ -11,5 +11,23 @@ describe('App', () => {
       )
       .toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  it('User should be able to go to Calculator page', () => {
+    render(<App />);
+    fireEvent.click(screen.getByText('Calculator'));
+    const result = screen.getByText('Let\'s do some math!');
+    expect(result.innerHTML).toBe('Let\'s do some math!');
+  });
+
+  it('User should be able to go to Calculator page and use calculator', () => {
+    render(<App />);
+    fireEvent.click(screen.getByText('Calculator'));
+    fireEvent.click(screen.getByText('5'));
+    fireEvent.click(screen.getByText('+'));
+    fireEvent.click(screen.getByText('5'));
+    fireEvent.click(screen.getByText('='));
+    const result = screen.getByText('10');
+    expect(result).toBeDefined();
   });
 });
